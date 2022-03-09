fcd0889b949a09d9b50dc0993864be755b9e38ba
Skye Wallace <sskwallace@Skyes-Air.hsd1.de.comcast.net>
2022-03-09 15:05:40 -0500

    added cors

--- db/migrate/20220309194531_create_projects.rb
+++ db/migrate/20220309194531_create_projects.rb
@@ -1,7 +1,12 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-
+      t.string :name
+      t.string :description
+      t.string :url
+      t.string :image_url
+      t.string :github_url
+      
       t.timestamps
     end
   end
---
+++ notes.md
@@ -0,0 +1,9 @@
+rails portfolio
+
+home controller linked to home view
+
+db migration for projects table
+has links to projects
+images
+description
+title
